1daa88044ff4d288225628c04143ec27448f673c
guilhermezils <guilhermezils@icloud.com>
2023-10-14 23:07:23 -0400

    _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Layout } from "../components";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+// import { StateContext } from "../../context/StateContext";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    // <StateContext>
+    <div>
+      <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </StateContext> */}
+    </div>
+  );
 }
+
+export default MyApp;

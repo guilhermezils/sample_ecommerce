908c3ed40496bb2773c89200720487a27a4dbc9e
guilhermezils <guilhermezils@icloud.com>
2023-10-17 22:22:21 -0400

    navbar1

---
+++ components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import { aiOutlineshopping } from "react-icons/ai";
+import { Cart } from "./";
+// import { useStateContext } from "../context/StateContext";
+
+const NavBar = () => {
+    // const { showcart, setShowCart, totalQuantities } = useStateContext();
+
+    return (
+        <div className="navbar-container">
+            <div className="company-name">
+                
+            </div>
+        </div> 
+    )
+}

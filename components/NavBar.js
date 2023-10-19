6d4dcf30b47c5c8d2590a8be639a3ec45bde9ec0
guilhermezils <guilhermezils@icloud.com>
2023-10-18 21:44:52 -0400

    navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,13 +5,29 @@ import { Cart } from "./";
 // import { useStateContext } from "../context/StateContext";
 
 const NavBar = () => {
-    // const { showcart, setShowCart, totalQuantities } = useStateContext();
+  // const { showcart, setShowCart, totalQuantities } = useStateContext();
 
-    return (
-        <div className="navbar-container">
-            <div className="company-name">
+  return (
+    <div className="navbar-container">
+      <div className="company-name">
+        <Link href="/">Mangez Macaron</Link>
+        <div className="navbar" logo={true}>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/shop">Shop</Link>
+          <Link href="/contact">Contact</Link>
+
+          {/* <button type="button" className="cart-icon" onClick={()=> setShowcart(true)}>
+                <AiOutlineShoppingCart />
+                <span className="cart-item-qty">{totalQuantities}</span>
+                </button>
+                {showcart && <Cart />} 
                 
-            </div>
-        </div> 
-    )
-}
+                */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;

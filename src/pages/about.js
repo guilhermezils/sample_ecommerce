ebc20c6650d267247bf21d3e296ce521b8e913a6
guilhermezils <guilhermezils@icloud.com>
2023-11-08 21:00:07 -0500

    about-us

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  <div className="about-us">
+    <p>About Macaron Magic</p>
+
+    <p>
+      Macarons are sweet meringue-based confections made with egg white, icing
+      sugar, granulated sugar, almond meal, and food coloring. They are light,
+      airy, and perfect for a sweet treat. Macaron Magic offers a variety of
+      flavors to satisfy every sweet tooth.
+    </p>
+
+    <p>
+      Our macarons come in a variety of flavors including vanilla, chocolate,
+      strawberry, pistachio, lemon, and raspberry. Each flavor is distinct and
+      delicious, offering a unique taste experience. Try them all and find your
+      favorite at Macaron Magic.
+    </p>
+  </div>;
+};
+
+export default About;

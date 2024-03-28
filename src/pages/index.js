2ba9445d6caaf2b9cd9f5dc73d2f5924c98cd25f
guilhermezils <guilhermezils@icloud.com>
2024-03-27 22:45:07 -0400

    blah

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-// import {client} from "../libs/client";
+import {client} from "../libs/client";
 import PerfectBanner from "@/components/PerfectBanner";
 import Newsletter from "@/components/Newsletter";
 

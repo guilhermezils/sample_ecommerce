64288d9bf51ac070af6928466551939b18982d69
guilhermezils <guilhermezils@icloud.com>
2024-04-28 21:33:43 -0400

    lib/client

--- sanity/sampleecommerce/sanity.config.js
+++ sanity/sampleecommerce/sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import product from './schemas/product'
 
 export default defineConfig({
   name: 'default',

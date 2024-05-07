59dd9f914997a8b65778928d001d2450591fa0a4
guilhermezils <guilhermezils@icloud.com>
2024-05-06 22:20:24 -0400

    final shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -11,9 +11,25 @@ const Shop = ({ products }) => (
       <p>Browse for products</p>
     </div>
     <div className="products-container">
-        {products?.map((product) => (
-            <Product key={product._id} product={product} />
-        ))}
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
     </div>
   </div>
 );
+
+export const getServerSideProps = async ({ req, res }) => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
+  return {
+    props: { products },
+  };
+};
+
+export default Shop;

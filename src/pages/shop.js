d135159676cb131c81d878cdbc6983f0613a6925
guilhermezils <guilhermezils@icloud.com>
2024-05-06 22:16:49 -0400

    shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,3 +2,18 @@ import React from "react";
 import { client } from "../../lib/client";
 
 import { Product } from "../components";
+
+const Shop = ({ products }) => (
+  <div>
+    <div className="products-heading">
+      <h2>Shop</h2>
+
+      <p>Browse for products</p>
+    </div>
+    <div className="products-container">
+        {products?.map((product) => (
+            <Product key={product._id} product={product} />
+        ))}
+    </div>
+  </div>
+);

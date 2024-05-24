a7703431b96874738cd87d5c2eb64905658bd465
guilhermezils <guilhermezils@icloud.com>
2024-05-23 23:07:21 -0400

    [slug].js part 2

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -25,8 +25,58 @@ const ProductDetails = ({ product, products }) => {
               className="product-detail-image"
             />
           </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                alt={name}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">
+            ${price.toLocaleString("en-US", {
+              maximumFractionDigits: 2,
+              minimumFractionDigits: 2,
+            })}
+          </p>
+          per box of 12
+          <div className="quantity-desc">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus">1</span>
+              <span className="plus">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="sku"> SKU: {sku}</div>
         </div>
       </div>
+
+
+
     </div>
   );
 };
+//    continue on step 7  
+

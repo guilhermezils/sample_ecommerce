f417330a4268c06e98d19cea373eef9099960867
guilhermezils <guilhermezils@icloud.com>
2024-05-28 23:00:54 -0400

    tabs component

---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+const info = ({ingredients, weight, delivery})=> (
+    <Tabs>
+        
+    </Tabs>
+)
+export default Info;

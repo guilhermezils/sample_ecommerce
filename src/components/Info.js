f4c7d28fbf0f3f08889a3cfc6eaf7ddbc0a6df26
guilhermezils <guilhermezils@icloud.com>
2024-05-29 22:39:54 -0400

    tabs component part 2

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-const info = ({ingredients, weight, delivery})=> (
-    <Tabs>
-        
-    </Tabs>
-)
+const info = ({ ingredients, weight, delivery }) => (
+  <Tabs>
+    <TabList>
+      <Tab>Description</Tab>
+      <Tab>Additional Information</Tab>
+    </TabList>
+    <TabPanel>
+      <h2>ingredients</h2>
+      <p>{ingredients}</p>
+    </TabPanel>
+    <TabPanel>
+      <h2>Additional Information</h2>
+      <table className="additional-info">
+        <tbody>
+          <tr>
+            <td>Weight:</td>
+            <td>{weight}</td>
+          </tr>
+          <tr>
+            <td>Delivery:</td>
+            <td>{delivery}</td>
+          </tr>
+        </tbody>
+      </table>
+    </TabPanel>
+  </Tabs>
+);
 export default Info;
